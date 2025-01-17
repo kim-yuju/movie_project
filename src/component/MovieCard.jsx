1ab5f3ff433f0b8dd99b94475a5fd49363a09c66
kim-yuju <revelation0829@naver.com>
2025-01-17 13:15:20 +0900

    misson1 카드클릭 추가

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./MovieCard.css"
+import { useNavigate } from "react-router-dom";
 
 const MovieCard = ({ vote_average, poster_path, title }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(`/details`)
+  }
+
   const base = "https://image.tmdb.org/t/p/w500";
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={handleClick}>
       <img src={`${base}${poster_path}`} alt="poster" className="movie-poster"/>
       <h2 className="movie-title">{title}</h2>
       <p className="movie-rating">{vote_average}</p>
